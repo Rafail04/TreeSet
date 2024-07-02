05062d35392dc3ac8c861658572c1bbfc0e79af2
Rafail <f3shooters@gmail.com>
2024-07-02 17:24:11 +0400

    Array.java

---
+++ src/Array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void Array (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int[] data = {1,2,3,4,5};
+        for (int i = 0; i < input-1 ; i++) {
+
+        }
+        if (input  <6 && input >0) {
+
+            System.out.println("true");
+
+        }else
+            System.out.println("false");
+
+    }
+}
+
+
+
